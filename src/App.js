7c97f3720ea2d0cd4d2bcb1858d203eae21b41e5
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2023-04-15 16:21:06 +0530

    fix 2
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { data } from './data/familyData';
 
 export default class App extends Component {
 
-  constructor(props) {
-    super(props);
 
+  
+  constructor(props) {
+
+    super(props);
   }
 
   render() {
